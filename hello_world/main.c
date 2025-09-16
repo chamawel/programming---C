3bb5bda9c89f45aaed3f879039b8d8c422b04962
Chamawel <ledentmael@gmail.com>
2025-09-16 21:05:38 +0200

    innitial commit

---
+++ hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main(){
+    printf("Hello world !");
+    int a = 5;
+    int b = 5;
+
+    int sum = a + b;
+    
+    printf("La somme de %d + %d est %d",a,b,sum);
+    return 0;
+}
